696da5fa2b2619130af0c3a8348e7faccf42fc0c
Isaiah Suarez <isaiahsuarez@fb.com>
2020-06-23 16:16:10 -0700

    Implemented local data persistence.

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -5,11 +5,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,20 +37,18 @@ public class MainActivity extends AppCompatActivity {
         etItem = findViewById(R.id.etItem);
         rvItems = findViewById(R.id.rvItems);
 
-
-
-        items = new ArrayList<>();
-        items.add("Buy milk");
-        items.add("Go to the Gym");
+        loadItems();
 
         ItemsAdapter.OnLongClickListener onLongClickListener = new ItemsAdapter.OnLongClickListener() {
             @Override
             public void onItemLongClicker(int position) {
                 // Delete the item from the model
                 items.remove(position);
+                saveItems();
                 // Notify the adapter
                 itemsAdapter.notifyItemRemoved(position);
                 Toast.makeText(getApplicationContext(), "Item was removed", Toast.LENGTH_SHORT).show();
+
             }
         };
 
@@ -58,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
                 String todoItem = etItem.getText().toString();
                 // Add to the model
                 items.add(todoItem);
+                saveItems();
                 // Notify the adapter that a new item was inserted
                 itemsAdapter.notifyItemInserted(items.size() - 1);
                 etItem.setText("");
@@ -65,5 +70,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private File getDataFile() {
+        return new File(getFilesDir(), "data.txt");
+    }
+
+    // Load items by reading every line of data.txt file
+    private void loadItems() {
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error reading items", e);
+            items = new ArrayList<>();
+        }
+    }
+
+    // Saves items by writing them into the data.txt file
+    private void saveItems() {
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error writing items", e);
+        }
     }
 }
